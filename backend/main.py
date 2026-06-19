c34aecd56158658ca18066ffe962bf3764abddf1
Aditya Kumar Jha <adityaceo007@gmail.com>
2026-06-19 10:05:05 +0530

    fix cors hardcoded

--- backend/main.py
+++ backend/main.py
@@ -26,7 +26,11 @@ if WEB_ORIGIN:
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://launchgate-pi.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
